a968851c39d88c594aec31baf2115b2440f1a674
Eric Madar <emsgeek@gmail.com>
2011-08-04 20:30:55 -0400

    Homepage created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ExpenseManager::Application.routes.draw do
+
   devise_for :users
 
+
+  root to: "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -58,3 +61,4 @@ ExpenseManager::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
+
